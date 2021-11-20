1f25ef8c1bff357da01b2fd817bcf8ce735734ff
root <root@ip-172-31-35-224.ap-south-1.compute.internal>
2021-11-20 08:01:18 +0000

    mybusinessaddedinmaven2

---
+++ mybusiness/src/main/java/com/surya/business/App.java
@@ -0,0 +1,13 @@
+package com.surya.business;
+
+/**
+ * Hai surya!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hai surya Welcome to maven second project!" );
+    }
+}
